f0cb050b6168644e543a3a5c0831169a98f5f240
Nuthan <inbox.nuthan@gmail.com>
2021-06-24 15:27:23 +0530

    new azure project - azure cosmos db

---
+++ azure_cosmos_db/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace azure_cosmos_db
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
