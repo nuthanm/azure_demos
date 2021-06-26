d272a6997b294019e68d344819b099db8bbe63d2
Nuthan <inbox.nuthan@gmail.com>
2021-06-26 21:44:54 +0530

    Code for azure blob

---
+++ azure_blob/azure_blob_demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace azure_blob_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
